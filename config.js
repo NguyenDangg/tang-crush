6945777e3867e8619689e07275797f7910f5c2cd
NguyenDangg <86768888+NguyenDangg@users.noreply.github.com>
2021-07-01 01:14:29 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Bae à!',
+    introDesc: `Đăngg có điều này muốn hỏi ngyeu
+    nhớ phải trả lời thật lòng nhaaa`,
+    btnIntro: 'OK',
+    title: 'Nếu cậu khum trả lời mà thoát ra tức là muốn làm vợ cụa Đănggg rùi đó nha :v',
+    desc: 'Cậu yêu tớ có phải không nào ._.',
+    btnYes: 'Yêu ơi là yêuuuuuuu <3',
+    btnNo: 'Đăng mơ à :3',
+    question:'Nói cho Đăng lí do cậu thích Đăngg đi :vvvv',
+    btnReply: 'Gửi cho Đăngggg <3',
+    reply: 'Vi Dangg dangiu va tot bung zl :<<<<<',
+    mess: 'Đănggg biết mà 🥰. Yêu iem nhiều nhiều 😘😘',
+    messDesc: 'Ngyeu còn chờ gì mà khong ib cho Đăngg đi nàoooooo',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
